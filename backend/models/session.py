6263f7036c719ced0c3dfa0ac7e7b86bb3c39514
megansin <megan_sin@student.wayland.k12.ma.us>
2026-01-19 00:11:37 -0500

    commit

--- backend/models/session.py
+++ backend/models/session.py
@@ -2,17 +2,24 @@ from pydantic import BaseModel, Field
 from datetime import datetime
 from typing import List, Optional
 
+# class Climb(BaseModel):
+#     grade: str
+#     hold_type: str
+#     angle: str
+#     result: str # e.g., "Flash", "Send", "Attempt"
+#     timestamp: datetime = Field(default_factory=datetime.utcnow)
+
 class Climb(BaseModel):
     grade: str
+    result: str
     hold_type: str
     angle: str
-    result: str # e.g., "Flash", "Send", "Attempt"
-    timestamp: datetime = Field(default_factory=datetime.utcnow)
+    attempts: int = 1
+    style: str = "Technical"
 
 class Session(BaseModel):
     username: str
     gym_id: str
-    # Optional because these are filled when the session ends
     start_time: datetime = Field(default_factory=datetime.utcnow)
     end_time: Optional[datetime] = None
     duration_minutes: Optional[float] = None
